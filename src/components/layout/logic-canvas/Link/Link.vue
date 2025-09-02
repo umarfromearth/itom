8307ecf1d0eb52fb67f6d9d200bad453907f5493
umar <umarfromearth@gmail.com>
2025-09-02 13:21:50 +0500

    exported: link code to link component and removed abundant components

---
+++ src/components/layout/logic-canvas/Link/Link.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+const { svg, link } = defineProps(["svg", "link"]);
+
+console.log(link, svg)
+
+const d = (link) => {
+    let x = svg.getBoundingClientRect().x;
+    let y = svg.getBoundingClientRect().y;
+
+    // C P1 P2 E
+    return `M ${link.sx - x} ${link.sy - y} C ${(link.sx - x + (Math.abs(link.ex - link.sx)) / 2)} ${link.sy - y} ${(link.ex - x - (Math.abs(link.ex - link.sx)) / 2)} ${link.ey - y} ${link.ex - x} ${link.ey - y}`;
+}
+</script>
+
+<template>
+    <path :d="d(link)" stroke="black" stroke-width="3" fill="none" @click="console.log(link.menu)" />
+</template>
+
+
+<style scoped>
+path:hover {
+    stroke-width: 5;
+}
+</style>
--- src/components/layout/logic-canvas/LinksBoard.vue/LinksBoard.vue
+++ src/components/layout/logic-canvas/LinksBoard.vue/LinksBoard.vue
@@ -1,30 +1,15 @@
 <script setup>
 import { useLinksStore } from '@/stores/global/linksStore';
-import { computed, useTemplateRef } from 'vue';
+import Link from '../Link/Link.vue'
+import { useTemplateRef } from 'vue';
 
 const linksStore = useLinksStore();
-
 const svg = useTemplateRef("svg");
 
-const d = ((link) => {
-    let x = svg.value.getBoundingClientRect().x;
-    let y = svg.value.getBoundingClientRect().y;
-
-    // C P1 P2 E
-
-    return `M ${link.sx - x} ${link.sy - y} C ${(link.sx - x + (Math.abs(link.ex - link.sx)) / 2)} ${link.sy - y} ${(link.ex - x - (Math.abs(link.ex - link.sx)) / 2)} ${link.ey - y} ${link.ex - x} ${link.ey - y}`;
-})
 </script>
 
 <template>
     <svg xmlns='https://www.w3.org/2000/svg' ref="svg">
-        <path v-for="link in linksStore.links" :d="d(link)" stroke="black" stroke-width="3" fill="none"
-            @click="console.log(link.menu)" />
+        <Link v-for="link in linksStore.links" :link="link" :svg="svg" />
     </svg>
 </template>
-
-<style scoped>
-path:hover {
-    stroke-width: 5;
-}
-</style>
